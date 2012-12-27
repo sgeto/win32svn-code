38538d10c1d1a1a13efde01a4d662dce66a86a56
David Darj <6233590+Alagazam@users.noreply.github.com>
2012-12-27 17:42:44 +0000

    Output Python version when building

--- BuildScript/set-python-ver.cmd
+++ BuildScript/set-python-ver.cmd
@@ -1,6 +1,6 @@
 @IF DEFINED %NOECHO% echo off
 
-echo ====== Set Python version environment variables ======
+echo ====== Set Python version environment variables version %PYTHONVER% ======
 set PYTHONVER=%1
 
 for /f "tokens=1,2 delims=/." %%a in ("%PYTHONVER%") do set PYTHONFILEVER=%%a%%b
