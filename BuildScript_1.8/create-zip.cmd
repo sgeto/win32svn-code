cef1112de02668ed6bbf93e075ccc41307b4fb67
alagazam <alagazam@b92b0119-d2c6-4c7a-8cff-0713d14c9694>
2013-06-18 20:30:00 +0000

    Log to separate files for Python bindings build (+minor log fixes)

--- BuildScript_1.8/create-zip.cmd
+++ BuildScript_1.8/create-zip.cmd
@@ -1,66 +1,66 @@
 @IF DEFINED NOECHO echo off
 
 echo ====== Create zip ======
-echo ====== Create zip ====== > %LOG_DIR%\create-zip.log
+echo ====== Create zip ====== > %LOG_DIR%\create-zip.%PYTHONVER%.log
 pushd %ROOT%
 set PATHORG=%PATH%
 PATH=%ROOT%\svn-win32-%VER%\svn-win32-%VER%\bin;%PATH%
 
-%AWKDIR%\awk.exe "{gsub(/bdbver = [0-9]*/,\"bdbver = %BDBVER%\"); };1" %DIR%\build\win32\make_dist.conf.template > %DIR%\build\win32\make_dist.conf.temp1 2>> %LOG_DIR%\create-zip.log
+%AWKDIR%\awk.exe "{gsub(/bdbver = [0-9]*/,\"bdbver = %BDBVER%\"); };1" %DIR%\build\win32\make_dist.conf.template > %DIR%\build\win32\make_dist.conf.temp1 2>> %LOG_DIR%\create-zip.%PYTHONVER%.log
 IF ERRORLEVEL 1 GOTO AWK_FAIL
-%AWKDIR%\awk.exe "{gsub(/zip.exe/,\"%ZIPEXE%\"); };1" %DIR%\build\win32\make_dist.conf.temp1 > %DIR%\build\win32\make_dist.conf.temp2 2>> %LOG_DIR%\create-zip.log
+%AWKDIR%\awk.exe "{gsub(/zip.exe/,\"%ZIPEXE%\"); };1" %DIR%\build\win32\make_dist.conf.temp1 > %DIR%\build\win32\make_dist.conf.temp2 2>> %LOG_DIR%\create-zip.%PYTHONVER%.log
 IF ERRORLEVEL 1 GOTO AWK_FAIL
-%AWKDIR%\awk.exe "{gsub(/jdk1.5.0_04/,\"jdk%JAVAVER%\"); };1" %DIR%\build\win32\make_dist.conf.temp2 > %DIR%\build\win32\make_dist.conf.temp3 2>> %LOG_DIR%\create-zip.log
+%AWKDIR%\awk.exe "{gsub(/jdk1.5.0_04/,\"jdk%JAVAVER%\"); };1" %DIR%\build\win32\make_dist.conf.temp2 > %DIR%\build\win32\make_dist.conf.temp3 2>> %LOG_DIR%\create-zip.%PYTHONVER%.log
 IF ERRORLEVEL 1 GOTO AWK_FAIL
-%AWKDIR%\awk.exe "{gsub(/C:\/Program Files\/ruby/,\"%RUBYDIRAWK%\"); };1" %DIR%\build\win32\make_dist.conf.temp3 > %DIR%\build\win32\make_dist.conf 2>> %LOG_DIR%\create-zip.log
+%AWKDIR%\awk.exe "{gsub(/C:\/Program Files\/ruby/,\"%RUBYDIRAWK%\"); };1" %DIR%\build\win32\make_dist.conf.temp3 > %DIR%\build\win32\make_dist.conf 2>> %LOG_DIR%\create-zip.%PYTHONVER%.log
 IF ERRORLEVEL 1 GOTO AWK_FAIL
 
 
 rem copy ReadMe to right folder
-copy /Y %SCRIPT_DIR%\README_ap%HTTPDVER%.txt %ROOT%\README.txt >> %LOG_DIR%\create-zip.log 2>>&1
+copy /Y %SCRIPT_DIR%\README_ap%HTTPDVER%.txt %ROOT%\README.txt >> %LOG_DIR%\create-zip.%PYTHONVER%.log 2>>&1
 IF ERRORLEVEL 1 GOTO COPY_FAIL
 
 rem ====== Regenerate VC project files - make_dist doesn't like junit-dir
 cd %ROOT%\%DIR%
-python gen-make.py -t dsp --with-httpd=..\%HTTPDDIR% --with-berkeley-db=%BDBDIR% --with-openssl=..\%HTTPDDIR%\srclib\openssl --with-zlib=..\%HTTPDDIR%\srclib\zlib --with-sasl=..\cyrus-sasl-%SASLVER% --enable-nls --with-libintl=..\svn-win32-libintl --with-serf=..\%DIR%\serf --with-swig=%SWIGDIR% --enable-bdb-in-apr-util --with-junit=%JUNITDIR%  >> %LOG_DIR%\create-zip.log 2>>&1
+python gen-make.py -t dsp --with-httpd=..\%HTTPDDIR% --with-berkeley-db=%BDBDIR% --with-openssl=..\%HTTPDDIR%\srclib\openssl --with-zlib=..\%HTTPDDIR%\srclib\zlib --with-sasl=..\cyrus-sasl-%SASLVER% --enable-nls --with-libintl=..\svn-win32-libintl --with-serf=..\%DIR%\serf --with-swig=%SWIGDIR% --enable-bdb-in-apr-util --with-junit=%JUNITDIR%  >> %LOG_DIR%\create-zip.%PYTHONVER%.log 2>>&1
 IF ERRORLEVEL 1 GOTO GENMAKE_FAIL
 
 cd %ROOT%
-python %DIR%\build\win32\make_dist.py --readme=%ROOT%\README.txt svn-win32-%VER% svn-win32-%VER% >> %LOG_DIR%\create-zip.log 2>>&1
+python %DIR%\build\win32\make_dist.py --readme=%ROOT%\README.txt svn-win32-%VER% svn-win32-%VER% >> %LOG_DIR%\create-zip.%PYTHONVER%.log 2>>&1
 IF ERRORLEVEL 1 GOTO MAKEDIST_FAIL
-del /Q svn-win32-%VER%\svn-win32-%VER%_py%PYTHONFILEVER%.zip >> %LOG_DIR%\create-zip.log 2>>&1
+del /Q svn-win32-%VER%\svn-win32-%VER%_py%PYTHONFILEVER%.zip >> %LOG_DIR%\create-zip.%PYTHONVER%.log 2>>&1
 IF ERRORLEVEL 1 GOTO MAKEDIST_FAIL
-ren svn-win32-%VER%\svn-win32-%VER%_py.zip svn-win32-%VER%_py%PYTHONFILEVER%.zip >> %LOG_DIR%\create-zip.log 2>>&1
+ren svn-win32-%VER%\svn-win32-%VER%_py.zip svn-win32-%VER%_py%PYTHONFILEVER%.zip >> %LOG_DIR%\create-zip.%PYTHONVER%.log 2>>&1
 IF ERRORLEVEL 1 GOTO MAKEDIST_FAIL
 
 popd
 PATH=%PATHORG%
 
 echo ====== Create zip success ======
-echo ====== Create zip success ====== >> %LOG_DIR%\create-zip.log
+echo ====== Create zip success ====== >> %LOG_DIR%\create-zip.%PYTHONVER%.log
 exit /B 0
 
 
 :AWK_FAIL
-echo ****** modification to make_dist.conf failed ****** >> %LOG_DIR%\create-zip.log
-type %LOG_DIR%\create-zip.log
+echo ****** modification to make_dist.conf failed ****** >> %LOG_DIR%\create-zip.%PYTHONVER%.log
+type %LOG_DIR%\create-zip.%PYTHONVER%.log
 popd
 exit /B 1
 
 :COPY_FAIL
-echo ****** copy of readme failed ****** >> %LOG_DIR%\create-zip.log
-type %LOG_DIR%\create-zip.log
+echo ****** copy of readme failed ****** >> %LOG_DIR%\create-zip.%PYTHONVER%.log
+type %LOG_DIR%\create-zip.%PYTHONVER%.log
 popd
 exit /B 1
 
 :GENMAKE_FAIL
-echo ****** genmake failed ****** >> %LOG_DIR%\create-zip.log
-type %LOG_DIR%\create-zip.log
+echo ****** genmake failed ****** >> %LOG_DIR%\create-zip.%PYTHONVER%.log
+type %LOG_DIR%\create-zip.%PYTHONVER%.log
 popd
 exit /B 1
 
 :MAKEDIST_FAIL
-echo ****** make dist failed ****** >> %LOG_DIR%\create-zip.log
-type %LOG_DIR%\create-zip.log
+echo ****** make dist failed ****** >> %LOG_DIR%\create-zip.%PYTHONVER%.log
+type %LOG_DIR%\create-zip.%PYTHONVER%.log
 popd
 exit /B 1
--- BuildScript_1.8/remove-zip.cmd
+++ BuildScript_1.8/remove-zip.cmd
@@ -4,17 +4,17 @@ echo ====== Remove zip ======
 echo ====== Remove zip ====== > %LOG_DIR%\remove-zip.log
 
 echo ------ Remove old dirs -----
-echo ------ Remove old dirs ----- >> %LOG_DIR%\create-zip.log
-rmdir /S /Q %ROOT%\svn-win32-%VER% >> %LOG_DIR%\create-zip.log 2>>&1
+echo ------ Remove old dirs ----- >> %LOG_DIR%\remove-zip.log
+rmdir /S /Q %ROOT%\svn-win32-%VER% >> %LOG_DIR%\remove-zip.log 2>>&1
 IF ERRORLEVEL 1 GOTO CLEAN_FAIL
 
 echo ====== Remove zip success ======
-echo ====== Remove zip success ====== >> %LOG_DIR%\create-zip.log
+echo ====== Remove zip success ====== >> %LOG_DIR%\remove-zip.log
 exit /B 0
 
 
 :CLEAN_FAIL
-echo ****** remove of old dirs failed ****** >> %LOG_DIR%\create-zip.log
-type %LOG_DIR%\create-zip.log
+echo ****** remove of old dirs failed ****** >> %LOG_DIR%\remove-zip.log
+type %LOG_DIR%\remove-zip.log
 popd
 exit /B 1
