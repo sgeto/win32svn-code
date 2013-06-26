d999e6fc6955e3f9f66692323e25ce84b8ff904a
alagazam <alagazam@b92b0119-d2c6-4c7a-8cff-0713d14c9694>
2013-06-26 12:33:49 +0000

    Updated WiX files for svn 1.7 build

--- Windows-WiX/BuildSubversion/WixDialog/build.bat
+++ Windows-WiX/BuildSubversion/WixDialog/build.bat
@@ -17,6 +17,7 @@ rem KIND, either express or implied.  See the License for the
 rem specific language governing permissions and limitations
 rem under the License.
 rem
-rem
-candle.exe -dinfoRtf=${uiextension.wixlib.dir.src}\Pre.rtf -dpostRtf=${uiextension.wixlib.dir.src}\Post.rtf WixUI_Subversion.wxs Infodlg.wxs WelcomeDlgSv.wxs PostDlg.wxs
-lit.exe -out WixUI_Subversion.wixlib WixUI_Subversion.wixobj infodlg.wixobj WelcomeDlgSv.wixobj PostDlg.wixobj
+set WIX_PATH=%ProgramFiles%\Windows Installer XML v3.5\bin
+"%WIX_PATH%\candle.exe" -dinfoRtf=${uiextension.wixlib.dir.src}\Pre.rtf -dpostRtf=${uiextension.wixlib.dir.src}\Post.rtf WixUI_Subversion.wxs Infodlg.wxs WelcomeDlgSv.wxs PostDlg.wxs
+"%WIX_PATH%\lit.exe" -out WixUI_Subversion.wixlib WixUI_Subversion.wixobj infodlg.wixobj WelcomeDlgSv.wixobj PostDlg.wixobj
+
