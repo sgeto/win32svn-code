3ebce3a28cab9167e11d3fe9c125f446c5afe05f
David Darj <6233590+Alagazam@users.noreply.github.com>
2013-08-27 06:42:23 +0000

    1.7-build: Updates for neon version 0.30.0

--- BuildScript/get-neon-source.cmd
+++ BuildScript/get-neon-source.cmd
@@ -12,6 +12,12 @@ echo %NEONVER%>%ROOT%\%DIR%\neon\.version
 %AWKDIR%\awk.exe "{gsub(/@VERSION@/,\"%NEONVER%\");gsub(/@MAJOR@/,\"%NEONMAJORVER%\");gsub(/@MINOR@/,\"%NEONMINORVER%\")};1" %ROOT%\%DIR%\neon\config.hw.in > %ROOT%\%DIR%\neon\config.hw 2>> %LOG_DIR%\get-neon-source.log
 IF ERRORLEVEL 1 GOTO PATCH_FAIL
 
+rem ****** patch error in ne_pkcs11 (ne_ssl.h doesn't have size_t defined in Neon 0.30.0)
+del %ROOT%\%DIR%\neon\src\ne_pkcs11.c.in >> %LOG_DIR%\get-neon-source.log 2>>&1
+ren %ROOT%\%DIR%\neon\src\ne_pkcs11.c ne_pkcs11.c.in >> %LOG_DIR%\get-neon-source.log 2>>&1
+%AWKDIR%\awk.exe "{gsub(/\"config.h\"/,\"\\\"config.h\\\"\n#include ^<stdlib.h^>\") };1" %ROOT%\%DIR%\neon\src\ne_pkcs11.c.in > %ROOT%\%DIR%\neon\src\ne_pkcs11.c 2>> %LOG_DIR%\get-neon-source.log
+IF ERRORLEVEL 1 GOTO PATCH_FAIL
+
 exit /B 0
 
 :CO_FAIL
