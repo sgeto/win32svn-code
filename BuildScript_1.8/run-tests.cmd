993e46d1d79c410945bdea86f43e2dfc025f1659
alagazam <alagazam@b92b0119-d2c6-4c7a-8cff-0713d14c9694>
2013-11-18 09:18:49 +0000

    Run all tests even if error

--- BuildScript_1.8/run-tests.cmd
+++ BuildScript_1.8/run-tests.cmd
@@ -14,8 +14,11 @@ echo ----- Test fsfs ------ >> %LOG_DIR%\run-tests.log
 echo ----- Logging tests to run-tests-fsfs.log ------ >> %LOG_DIR%\run-tests.log
 echo ----- Test fsfs ------ > %LOG_DIR%\run-tests-fsfs.log
 python win-tests.py -c -r -v >> %LOG_DIR%\run-tests-fsfs.log 2>>&1
-:IF ERRORLEVEL 1 GOTO TEST_FAIL
-::goto SUCCESS
+IF ERRORLEVEL 1 GOTO fsfstest_fail
+goto fsfssuccess
+:fsfstest_fail
+set FSFSERROR=1
+:fsfssuccess
 
 :bdb
 echo ----- Test bdb ------
@@ -23,8 +26,11 @@ echo ----- Test bdb ------ >> %LOG_DIR%\run-tests.log
 echo ----- Logging tests to run-tests-bdb.log ------ >> %LOG_DIR%\run-tests.log
 echo ----- Test bdb ------ > %LOG_DIR%\run-tests-bdb.log
 python win-tests.py -c -r -v -f bdb >> %LOG_DIR%\run-tests-bdb.log 2>>&1
-:IF ERRORLEVEL 1 GOTO TEST_FAIL
-::goto SUCCESS
+IF ERRORLEVEL 1 GOTO bdbtest_fail
+goto bdbsuccess
+:bdbtest_fail
+set BDBERROR=1
+:bdbsuccess
 
 :svn
 echo ----- Test svn ------
@@ -34,9 +40,12 @@ echo ----- Test svn ------ > %LOG_DIR%\run-tests-svn.log
 netsh firewall add allowedprogram "%ROOT%\%DIR%\Release\subversion\svnserve\svnserve.exe" svnserve-%VER%-%HTTPDVER% >> %LOG_DIR%\run-tests.log 2>>&1
 IF ERRORLEVEL 1 GOTO TEST_FAIL
 python win-tests.py -c -r -v -u svn://localhost >> %LOG_DIR%\run-tests-svn.log 2>>&1
-:IF ERRORLEVEL 1 GOTO TEST_FAIL
+IF ERRORLEVEL 1 GOTO svntest_fail
+goto svnsuccess 
+:svntest_fail
+set SVNERROR=1
+:svnsuccess
 %SYSINTERNALS%\pskill.exe -t svnserve.exe
-::goto SUCCESS
 
 :httpd
 echo ----- Test http-serf ------
@@ -46,8 +55,17 @@ echo ----- Test http-serf ------ > %LOG_DIR%\run-tests-http-serf.log
 start "HTTPD" "%HTTPDINSTDIR%\bin\httpd.exe" >> %LOG_DIR%\run-tests.log
 IF ERRORLEVEL 1 GOTO TEST_FAIL
 python win-tests.py -c -r -v -u http://localhost:80%HTTPDVER% >> %LOG_DIR%\run-tests-http-serf.log 2>>&1
-:IF ERRORLEVEL 1 GOTO TEST_FAIL
-::goto SUCCESS
+IF ERRORLEVEL 1 GOTO httptest_fail
+goto httpsuccess 
+:httptest_fail
+set HTTPERROR=1
+:httpsuccess
+%SYSINTERNALS%\pskill.exe -t httpd.exe
+
+if defined FSFSERROR goto TEST_FAIL
+if defined BDBERROR goto TEST_FAIL
+if defined SVNERROR goto TEST_FAIL
+if defined HTTPERROR goto TEST_FAIL
 
 :SUCCESS
 PATH=%PATHORG%
@@ -58,6 +76,11 @@ exit /B 0
 
 :TEST_FAIL
 echo ****** Tests failed ****** >> %LOG_DIR%\run-tests.log
+if defined FSFSERROR type %LOG_DIR%\run-tests-fsfs.log
+if defined BDBERROR type %LOG_DIR%\run-tests-bdb.log
+if defined SVNERROR type %LOG_DIR%\run-tests-svn.log
+if defined HTTPERROR type %LOG_DIR%\run-tests-http-serf.log
+
 type %LOG_DIR%\run-tests.log
 PATH=%PATHORG%
 popd
