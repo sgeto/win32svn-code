019403131c564fa19f1f2c6c3c2faa13b992e146
alagazam <alagazam@b92b0119-d2c6-4c7a-8cff-0713d14c9694>
2014-08-27 10:56:22 +0000

    Updates for 1.7.18 build

--- BuildScript/README_ap22.txt
+++ BuildScript/README_ap22.txt
@@ -1,4 +1,4 @@
-This is Subversion for Win32, version 1.7.17-1. Read the CHANGES file to
+This is Subversion for Win32, version 1.7.18. Read the CHANGES file to
 see what changed in this release:
 
     http://svn.apache.org/repos/asf/subversion/branches/1.7.x/CHANGES
@@ -10,9 +10,9 @@ These binaries were built with:
     APR-ICONV 1.2.1
     Neon 0.30.0
     Berkeley DB 4.8.30
-    OpenSSL 1.0.1h
+    OpenSSL 1.0.1i
     ZLib 1.2.8
-    Apache 2.2.26
+    Apache httpd 2.2.27
 	
     Python 2.5.4 , 2.6.6 and 2.7.6 (2.5.6 and 2.6.9 is source only releases)
     Perl 5.16.3 (ActivePerl)
@@ -21,7 +21,7 @@ These binaries were built with:
     Ruby 1.8.6
     Cyrus SASL 2.1.23
     serf 1.2.1
-    sqlite 3.8.3.1
+    sqlite 3.8.6
     SWIG 1.3.40
 
 The patched libintl is at
--- BuildScript/run-tests.cmd
+++ BuildScript/run-tests.cmd
@@ -6,17 +6,21 @@ set PATHORG=%PATH%
 PATH=%ROOT%\svn-win32-%VER%\svn-win32-%VER%\bin;%PATH%
 pushd %ROOT%\%DIR%
 
+::goto BDB
 
+:FSFS
 echo ----- Test fsfs ------
 echo ----- Test fsfs ------ >> %LOG_DIR%\run-tests.log
 python win-tests.py -c -r -v >> %LOG_DIR%\run-tests.log 2>>&1
 IF ERRORLEVEL 1 GOTO TEST_FAIL
 
+:BDB
 echo ----- Test bdb ------
 echo ----- Test bdb ------ >> %LOG_DIR%\run-tests.log
 python win-tests.py -c -r -v -f bdb >> %LOG_DIR%\run-tests.log 2>>&1
 IF ERRORLEVEL 1 GOTO TEST_FAIL
 
+:SVN
 echo ----- Test svn ------
 echo ----- Test svn ------ >> %LOG_DIR%\run-tests.log
 netsh firewall add allowedprogram "%ROOT%\%DIR%\Release\subversion\svnserve\svnserve.exe" svnserve-%VER%-%HTTPDVER% >> %LOG_DIR%\run-tests.log 2>>&1
@@ -25,18 +29,20 @@ python win-tests.py -c -r -v -u svn://127.0.0.1 >> %LOG_DIR%\run-tests.log 2>>&1
 IF ERRORLEVEL 1 GOTO TEST_FAIL
 %SYSINTERNALS%\pskill.exe -t svnserve.exe
 
-
+:NEON
 start "HTTPD" "%HTTPDINSTDIR%\bin\httpd.exe"
 echo ----- Test http-neon ------
 echo ----- Test http-neon ------ >> %LOG_DIR%\run-tests.log
 python win-tests.py -c -r -v -u http://127.0.0.1:80%HTTPDVER% --http-library=neon >> %LOG_DIR%\run-tests.log 2>>&1
 IF ERRORLEVEL 1 GOTO TEST_FAIL
 
+:SERF
 echo ----- Test http-serf ------
 echo ----- Test http-serf ------ >> %LOG_DIR%\run-tests.log
 python win-tests.py -c -r -v -u http://127.0.0.1:80%HTTPDVER% --http-library=serf >> %LOG_DIR%\run-tests.log 2>>&1
 IF ERRORLEVEL 1 GOTO TEST_FAIL
 
+:END
 PATH=%PATHORG%
 popd
 exit /B 0
