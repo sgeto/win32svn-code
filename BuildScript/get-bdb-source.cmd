57980baf3396e79a0ba115d59d482391aa797593
David Darj <6233590+Alagazam@users.noreply.github.com>
2014-12-19 07:26:23 +0000

    Updates for 1.7.19 build

--- BuildScript/get-bdb-source.cmd
+++ BuildScript/get-bdb-source.cmd
@@ -2,7 +2,7 @@
 
 echo ====== Get bdb Source code ======
 echo ====== Get bdb Source code  ====== > %LOG_DIR%\get-bdb-source.log
-%WGET% -N --no-check-certificate --directory-prefix=%DLDIR% --no-check-certificate http://download.oracle.com/berkeley-db/db-%BDBFULLVER%.zip >> %LOG_DIR%\get-bdb-source.log 2>>&1
+%WGET% -N --directory-prefix=%DLDIR% --no-check-certificate http://download.oracle.com/berkeley-db/db-%BDBFULLVER%.zip >> %LOG_DIR%\get-bdb-source.log 2>>&1
 IF ERRORLEVEL 1 GOTO CO_FAIL
 
 echo ====== Extract bdb ======
